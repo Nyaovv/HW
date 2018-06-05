050b9e5d508304f6e8726799d02b113cd7fd3147
Nyaovv <plusfak@gmail.com>
2018-06-06 02:00:08 +0300

    Done

--- task-validator.py
+++ task-validator.py
@@ -41,11 +41,37 @@ class EMailValidator(Validator):
         return True
 
 class DateTimeValidator(Validator):
+    def data_checker(self, d_value):
+        if "-" in d_value:
+            d_value = d_value.split("-")
+        elif "/" in d_value:
+            d_value = d_value.split("/")
+        elif "." in d_value:
+            d_value = d_value.split(".")
+
+        if int(d_value[0]) in range(1, 9999) and int(d_value[1]) in range(1, 13) and int(d_value[2]) in range(1, 32): #проверка с какой части начинается год
+            return True
+        if int(d_value[0]) in range(1, 32) and int(d_value[1]) in range(1, 13) and int(d_value[2]) in range(1, 9999):
+            return True
+        return False
+
+    def time_checker(self, t_value):
+        t_value = t_value.split(":")
+        if len(t_value) == 2:
+            if int(t_value[0]) in range(24) and int(t_value[1]) in range(61):
+                return True
+        elif len(t_value) == 3:
+            if int(t_value[0]) in range(24) and int(t_value[1]) in range(61) and int(t_value[2]) in range(61):
+                return True
+        return False
+
     def validate(self, value):
-        pass
-
-
-Validator.add_type('email', EMailValidator)
-validator = Validator.get_instance('email')
-#print(validator.validate('kak@kak.com'))
-#print(Validator.types)
+        value = value.split(" ")
+        if len(value) == 1:
+            if self.data_checker(value[0]):
+                return True
+        if len(value) == 2:
+            if self.data_checker(value[0]) and self.time_checker(value[1]):
+                #print('t2')
+                return True
+        return True
