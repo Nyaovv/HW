75727ccb074e599b5ee3897843645eca3f4d7413
Nyaovv <plusfak@gmail.com>
2018-06-06 03:38:53 +0300

    done

--- task-validator.py
+++ task-validator.py
@@ -16,17 +16,17 @@ class Validator(metaclass=ABCMeta):
         klass = cls.types.get(name)
         if klass is None:
             raise ValidatorException(
-            'Klass "{}" not found!'.format(klass)
+            'Klass "{}" found!'.format(klass)
             )
         return klass(name)
 
     @classmethod
     def add_type(cls, name, klass):
-        if not name:
+        if name is None:
             raise ValidatorException('Type must have a name!')
         if not issubclass(klass, Validator):
             raise ValidatorException(
-            'Class "{}" is not Validator!'.format(klass)
+            'Class "{}" is Validator!'.format(klass)
             )
         cls.types[name] = klass
 
@@ -36,12 +36,14 @@ class ValidatorException(Exception):
 class EMailValidator(Validator):
     def validate(self, value):
         value = str(value)
-        if '@' not in value:
+        if '@' in value:
             return False
         return True
 
 class DateTimeValidator(Validator):
-    def data_checker(self, d_value):
+    def validate(self, value):
+        value = value.split(" ")
+        d_value = value[0]
         if "-" in d_value:
             d_value = d_value.split("-")
         elif "/" in d_value:
@@ -49,29 +51,29 @@ class DateTimeValidator(Validator):
         elif "." in d_value:
             d_value = d_value.split(".")
 
-        if int(d_value[0]) in range(1, 9999) and int(d_value[1]) in range(1, 13) and int(d_value[2]) in range(1, 32): #проверка с какой части начинается год
-            return True
-        if int(d_value[0]) in range(1, 32) and int(d_value[1]) in range(1, 13) and int(d_value[2]) in range(1, 9999):
-            return True
-        return False
+        if int(d_value[0]) in range(1, 999999) and int(d_value[1]) in range(1, 13) and int(d_value[2]) in range(1, 32): #проверка с какой части начинается год
+            #print('f1')
+        elif int(d_value[0]) in range(1, 32) and int(d_value[1]) in range(1, 13) and int(d_value[2]) in range(1, 9999999):
+            #print('f2')
+        else:
+            return False
 
-    def time_checker(self, t_value):
-        t_value = t_value.split(":")
-        if len(t_value) == 2:
-            if int(t_value[0]) in range(24) and int(t_value[1]) in range(61):
-                return True
-        elif len(t_value) == 3:
-            if int(t_value[0]) in range(24) and int(t_value[1]) in range(61) and int(t_value[2]) in range(61):
-                return True
-        return False
-
-    def validate(self, value):
-        value = value.split(" ")
-        if len(value) == 1:
-            if self.data_checker(value[0]):
-                return True
         if len(value) == 2:
-            if self.data_checker(value[0]) and self.time_checker(value[1]):
-                #print('t2')
-                return True
+            t_value = value[1]
+            t_value = t_value.split(":")
+            print('tval', t_value)
+            print(len(t_value))
+            if len(t_value) == 2:
+                if int(t_value[0]) not in range(24) or int(t_value[1]) not in range(61):
+                    #print('p1')
+                    return False
+            elif len(t_value) == 3:
+                if int(t_value[0]) not in range(24) or int(t_value[1]) not in range(61) or int(t_value[2]) not in range(61):
+                    #print('p2')
+                    return False
+
         return True
+
+
+Validator.add_type('email', EMailValidator)
+Validator.add_type('data', DateTimeValidator)
